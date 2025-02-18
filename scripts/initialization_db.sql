a2230a47de331a77a71bec3603578072eb06fe14
Ioannis Chondros <ioannischondros22@gmail.com>
2025-02-19 01:33:33 +0200

    Update initialization_db.sql

--- scripts/initialization_db.sql
+++ scripts/initialization_db.sql
@@ -1,14 +1,20 @@
 /* 
-=====================================================================
+==================================================================================
 Create Database and Schemas
-=====================================================================
+==================================================================================
 
 Script Purpose:
-	This script creates a new database named 'Datawarehouse' after checking if it already exists. If it does exist, it is dropped and created once again. Moreover, this script sets up three schemas in the database with the distinctive names: 'bronze', 'silver' & 'gold'.
+	This script creates a new database named 'Datawarehouse' after checking 
+	if it already exists. If it does exist, it is dropped and created once 
+	again. Moreover, this script sets up three schemas in the database with 
+	the distinctive names: 'bronze', 'silver' & 'gold'.
 
 WARNING:
-	Running this particular script will drop the entire 'Datawarehouse' database if it exists. All data in the database will be DELETED permanently. Make sure you have backups before you proceed. 
+	Running this particular script will drop the entire 'Datawarehouse' 
+	database if it exists. All data in the database will be DELETED 
+	permanently. Make sure you have backups before you proceed. 
 
+==================================================================================
 */
 
 
